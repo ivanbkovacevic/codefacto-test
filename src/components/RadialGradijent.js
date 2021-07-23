6c25f2079eb7ff797b4af515dde35cf46b338a89
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2021-07-23 22:46:37 +0200

    vs

--- src/components/RadialGradijent.js
+++ src/components/RadialGradijent.js
@@ -6,7 +6,6 @@ const RadialGradijent = () => {
         <div className="radial-container">
            <p>radial gradijen</p> 
         </div>
-<<<<<<< HEAD
         <div className="multiple-gradient">o</div>
         main branch
         main branch
@@ -14,13 +13,11 @@ const RadialGradijent = () => {
         main branch
         main branch
         main branch
-=======
         <div className="multiple-gradient"></div>
         <p>grana1</p>
         <p>grana1 1commit</p>
         <p>grana1 2commit</p>
 
->>>>>>> 74dbff1cc7a6f2c7666e95707b48ee79eb64a8ec
         </>
     );
 };
