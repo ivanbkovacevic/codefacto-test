3c6a33b7457dc805f91a0616c1cc277bc2b185ee
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2021-07-23 22:47:36 +0200

    dvsvws

--- src/components/RadialGradijent.js
+++ src/components/RadialGradijent.js
@@ -9,7 +9,7 @@ const RadialGradijent = () => {
         <div className="multiple-gradient"></div>
         <p>grana1</p>
         <p>grana1 1commit</p>
-        <p>grana1 2commit</p>
+        <p>ggiorana1 2commit</p>
 
         grana 1 ponovo commit
         grana 1 ponovo commit
